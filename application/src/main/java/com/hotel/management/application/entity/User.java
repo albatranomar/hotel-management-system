47f16efcb157826a2f385f17c2c0dc04ea16bf00
albatranomar <albatranomar943@gmail.com>
2024-06-11 21:02:54 +0300

    fix(config): fix typo

---
+++ application/src/main/java/com/hotel/management/application/entity/User.java
@@ -0,0 +1,20 @@
+package com.hotel.management.application.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+    @Id
+    private String id;
+
+    private String role;
+
+    private String firstName, lastName, email, password;
+}
