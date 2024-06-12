8e512a15393bb541130ea8b363ab2c763b4016ff
Baha Al-Khateeb <babaalkhateeb@gmail.com>
2024-06-12 10:35:16 +0200

    feat(Api-dto): Added Payment and Booking dtos.

---
+++ application/src/main/java/com/hotel/management/application/dto/BookingDto.java
@@ -0,0 +1,11 @@
+package com.hotel.management.application.dto;
+
+import java.util.List;
+
+public class BookingDto {
+    private String id;
+
+    private UserDto user;
+
+    private List<RoomDto> rooms;
+}
---
+++ application/src/main/java/com/hotel/management/application/dto/PaymentDto.java
@@ -0,0 +1,8 @@
+package com.hotel.management.application.dto;
+
+public class PaymentDto {
+    private String id;
+    private String paymentStatus;
+
+    private BookingDto booking;
+}
