f367e24267d93b7256d4e684e1ec09e878c13a73
albatranomar <albatranomar943@gmail.com>
2024-06-12 10:49:49 +0300

    feat(api-service): Add BookingService

---
+++ application/src/main/java/com/hotel/management/application/service/BookingService.java
@@ -0,0 +1,16 @@
+package com.hotel.management.application.service;
+
+import com.hotel.management.application.dto.BookingDto;
+import com.hotel.management.application.dto.RoomDto;
+
+import java.util.List;
+
+public interface BookingService {
+    List<BookingDto> getAllBookings();
+    BookingDto getBookingById();
+    BookingDto updateBookingById(String id, BookingDto bookingDto);
+    void deleteProviderById(String id);
+    BookingDto createBooking(BookingDto bookingDto);
+    boolean bookingExistsWithId(String id);
+    List<RoomDto> getBookingRooms(String bookingId);
+}
