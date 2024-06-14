685b1a4af8dc5d5474e2828c8958a32952beba67
albatranomar <albatranomar943@gmail.com>
2024-06-14 09:59:02 +0300

    feat(api-auth&db): JWT filter implementation & introduce Token entity to the database

---
+++ application/src/main/java/com/hotel/management/application/entity/user/Token.java
@@ -0,0 +1,38 @@
+package com.hotel.management.application.entity.user;
+
+import com.hotel.management.application.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    public String id;
+
+    @Column(unique = true)
+    public String token;
+
+    @Enumerated(EnumType.STRING)
+    public TokenType tokenType = TokenType.BEARER;
+
+    public boolean revoked;
+
+    public boolean expired;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    public User user;
+
+    public enum TokenType {
+        BEARER
+    }
+}
