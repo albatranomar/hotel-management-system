f21db988c12d31e40982198e11295881825ab426
Baha Al-Khateeb <babaalkhateeb@gmail.com>
2024-06-17 14:00:51 +0200

    fix(Api-advise): Changed to the right AccessDeniedException (spring security one) from the file one.

--- application/src/main/java/com/hotel/management/application/advise/GlobalAdvise.java
+++ application/src/main/java/com/hotel/management/application/advise/GlobalAdvise.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -20,9 +21,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.awt.image.RescaleOp;
-import java.nio.file.AccessDeniedException;
 
 @ControllerAdvice
 public class GlobalAdvise extends ResponseEntityExceptionHandler {
